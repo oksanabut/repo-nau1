b677cde391a55719e4a4b91dc73fde9a1c6f55a7
OKSANA KLYMENKO <a2107629@montilivi.cat>
2022-05-03 20:51:09 +0200

    Initial of project

---
+++ Nau.py
@@ -0,0 +1,22 @@
+class Nau:
+
+    def __init__(self,x,y):        
+        self.x=x
+        self.y=y
+        self.incr=1
+    
+    def moure_dreta():
+        pass
+        
+    def moure_esquerre():
+        pass
+
+    def moure_amunt():
+        pass
+        
+    def moure_avall():
+        pass    
+        
+    def mostrar_nau():
+        pass
+
