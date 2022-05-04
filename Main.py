99076456d69cd25fef0eb556c9ddd6049a0889e2
OKSANA KLYMENKO <a2107629@montilivi.cat>
2022-05-04 20:45:07 +0200

    Modificaciones de python

--- Main.py
+++ Main.py
@@ -17,16 +17,21 @@ def main():
     while not sortir:
         op = input('Entra una opció')
         if op=='d':
+            nau.moure_dreta
             print('moure a la dreta')
         elif op=='a':
+            nau.moure_esquerre
             print('moure a l''esquerre')
         elif op=='w':
+            nau.moure_amunt
             print('moure a amunt')
         elif op=='s':
+            nau.moure_avall
             print('moure a avall')
         elif op=='0':
             sortir=True
             print("Has sortit de l'avió")
-        
+        nau.mostrar_nau()
+
 if __name__ == "__main__":
     main()
